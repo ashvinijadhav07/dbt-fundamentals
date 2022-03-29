bf467d02d0dfda8fa28b6f8bcc22be259e222255
ashvini.jadhav07@gmail.com <ashvini.jadhav07@gmail.com>
2022-03-29 09:07:13 +0000

    dim_customers commit

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,16 +1,28 @@
 {{
-    config(materialized="table")
+    config(
+        materialized="table"
+    )
 }}
-
 with customers as (
 
-    select * from {{ ref('stg_customers') }}
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from dbt-tutorial.jaffle_shop.customers
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from dbt-tutorial.jaffle_shop.orders
 
 ),
 
